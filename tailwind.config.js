9f4447f47d05656dae6b39114d3bdfaada1a470f
edwintantawi <edwintantawi@gmail.com>
2021-06-03 12:18:49 +0700

    config tailwindcss custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      signature: ['Great Vibes', 'cursive'],
+    },
     colors: {
       gray: '#818181',
       'light-gray': '#D9D9D9',
