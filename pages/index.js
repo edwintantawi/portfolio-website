28ba63413a389960a7225dd2caf12eb10927450d
edwintantawi <edwintantawi@gmail.com>
2021-06-03 12:19:05 +0700

    try to use fontawsome

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 const Home = () => {
   return (
     <h1 className="max-w-[200px] mx-auto text-center text-black">
-      Portfolio Website with Next JS + Tailwindcss
+      Portfolio Website with Next JS + Tailwindcss{' '}
+      <i className="far fa-heart" />
     </h1>
   );
 };
