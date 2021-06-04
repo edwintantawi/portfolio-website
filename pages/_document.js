232dc4a51ed901b5fb07d4ea32090e8f6c860b4a
edwintantawi <edwintantawi@gmail.com>
2021-06-04 16:06:31 +0700

    fixing typo

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
             integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
