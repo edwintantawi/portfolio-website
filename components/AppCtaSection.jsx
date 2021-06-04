0800af2a12b7136ace62f4046f14ad629439e4cb
edwintantawi <edwintantawi@gmail.com>
2021-06-04 16:04:46 +0700

    create AppCtaAction

---
+++ components/AppCtaSection.jsx
@@ -0,0 +1,20 @@
+import AppButton from './AppButton';
+import AppDecoration from './AppDecoration';
+import AppSection from './AppSection';
+
+const AppCtaSection = () => {
+  return (
+    <AppSection title="GET IN TOUCH">
+      <div className="text-center">
+        <p className="text-xs md:text-sm text-gray max-w-[260px] md:max-w-xs mx-auto ">
+          Is there anything I can help you with? My inbox and social media are
+          always open to you, feel free to ask, I will try my best for you.
+          Thank you for visiting.
+        </p>
+        <AppButton title="Say Hi.." href="/contact" />
+      </div>
+    </AppSection>
+  );
+};
+
+export default AppCtaSection;
