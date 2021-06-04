98777a67d083f123d574cc4b84e1f9fa1f5ab5df
edwintantawi <edwintantawi@gmail.com>
2021-06-04 12:16:20 +0700

    create AppDecoration

---
+++ components/AppDecoration.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+const AppDecoration = ({ type }) => {
+  switch (type) {
+    case 'fixline':
+      return (
+        <div className="fixed z-[-1] w-[80%] 2xl:w-[1200px] transform right-1/2 translate-x-1/2">
+          <div className="decoration h-screen w-full" />
+        </div>
+      );
+    case 'threeline':
+      return (
+        <div className="text-center mt-2">
+          <span className="inline-block bg-orange w-[3px] h-7" />
+          <span className="inline-block bg-black w-[3px] h-7 mx-[3px] -mb-2" />
+          <span className="inline-block bg-orange w-[3px] h-7" />
+        </div>
+      );
+    default:
+      return <p>Invalid props</p>;
+  }
+};
+
+AppDecoration.propTypes = {
+  type: PropTypes.oneOf(['fixline', 'threeline']),
+};
+
+AppDecoration.defaultProps = {
+  type: 'fixline',
+};
+
+export default AppDecoration;
