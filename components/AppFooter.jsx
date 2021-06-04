311a2980a49215e2bcf7552ab34c92e72e70b74a
edwintantawi <edwintantawi@gmail.com>
2021-06-04 16:05:14 +0700

    create AppFooter

---
+++ components/AppFooter.jsx
@@ -0,0 +1,22 @@
+import USER from '../data/user.json';
+
+const AppFooter = () => {
+  const handleCopyrightYears = () => {
+    const startYears = '2021';
+    const nowYears = new Date().getFullYear();
+    const rangeYears =
+      startYears === nowYears.toString()
+        ? startYears
+        : `${startYears} - ${nowYears}`;
+    return rangeYears;
+  };
+
+  return (
+    <footer className="text-center text-xs md:text-md text-gray py-7">
+      <p>Design & Build With &#128149; by {USER.name}</p>
+      <p>copyright &copy; {handleCopyrightYears()}</p>
+    </footer>
+  );
+};
+
+export default AppFooter;
