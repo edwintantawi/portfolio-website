8161882eea67cac35f61bb83217f23337a893dd1
edwintantawi <edwintantawi@gmail.com>
2021-06-05 23:46:18 +0700

    adding Timeline tree

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -5,6 +5,10 @@ import AppDecoration from '../components/AppDecoration';
 import AppSide from '../components/AppSide';
 import AppContainer from '../components/AppContainer';
 import AppHeader from '../components/AppHeader';
+import AppSection from '../components/AppSection';
+import AppTimeline from '../components/AppTimeline';
+import AppCtaSection from '../components/AppCtaSection';
+import AppFooter from '../components/AppFooter';
 
 const Resume = () => {
   return (
@@ -18,6 +22,21 @@ const Resume = () => {
           title={USER.resume.title}
           description={USER.resume.description}
         />
+        {USER.resume.contents.map((item) => (
+          <AppSection key={item.id} title={item.title}>
+            {item.contents.map((subItem) => (
+              <AppTimeline
+                timeline={subItem.timestamp}
+                title={subItem.name}
+                subTitle={subItem.info}
+              >
+                {subItem.description}
+              </AppTimeline>
+            ))}
+          </AppSection>
+        ))}
+        <AppCtaSection />
+        <AppFooter />
       </AppContainer>
     </div>
   );
