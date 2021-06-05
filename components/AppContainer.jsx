e33fe276b7225df4823ab747bdf0e2bf4dfab086
edwintantawi <edwintantawi@gmail.com>
2021-06-05 22:10:31 +0700

    fix symantic element and z-index

--- components/AppContainer.jsx
+++ components/AppContainer.jsx
@@ -4,7 +4,9 @@ const AppContainer = ({ children }) => {
   return (
     <div className="max-w-[1430px] mx-auto md:px-36">
       <div className="max-w-[1140px] xl:border-light-gray xl:border-r xl:border-l ">
-        <div className="max-w-[1000px] mx-auto px-3">{children}</div>
+        <main className="max-w-[1000px] mx-auto px-3 relative z-20">
+          {children}
+        </main>
       </div>
     </div>
   );
