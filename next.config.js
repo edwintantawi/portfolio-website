6a702f3a8e1b512eebf76cf14b81619ea03a5829
edwintantawi <edwintantawi@gmail.com>
2021-06-18 17:55:42 +0700

    disable next-pwa in development

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPwa = require('next-pwa');
 module.exports = withPwa({
   pwa: {
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
   },
   images: {
     domains: ['firebasestorage.googleapis.com'],
