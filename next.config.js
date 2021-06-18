f9d8e9753c5b0e4c414e85d54856c0b336ddd40d
edwintantawi <edwintantawi@gmail.com>
2021-06-18 17:33:51 +0700

    use next-pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withPwa = require('next-pwa');
+
+module.exports = withPwa({
+  pwa: {
+    dest: 'public',
+  },
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-};
+});
