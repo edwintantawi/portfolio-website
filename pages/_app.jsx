d7c7a6ea75af592a43e86d8677dedf86fae3a3a0
edwintantawi <edwintantawi@gmail.com>
2021-07-13 10:39:38 +0700

    ✨ run firebase analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,15 @@
+import { useEffect } from 'react';
+import { analytics } from '../lib/firebase';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      analytics();
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
+};
 
 export default MyApp;
