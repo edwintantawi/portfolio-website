fb98c7e7a6803cd94a4621d90621e687261f8a11
edwintantawi <edwintantawi@gmail.com>
2021-08-07 22:21:03 +0700

    ✏️ fix typo

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,16 +8,16 @@ import AppShell from '../components/templates/AppShell';
 const Home = () => (
   <div>
     <AppShell hero>
-      <AppSection title="What can i do">
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-4">
+      <AppSection title="What can I do">
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3 lg:gap-4">
           {USER.services.map((item) => (
             <div
               key={item.id}
-              className="border border-light-gray rounded-lg p-6"
+              className="p-6 border rounded-lg border-light-gray"
             >
               <div className="flex items-center">
                 <Image src={item.icon} alt={item.name} width={40} height={40} />
-                <h3 className="text-black text-sm lg:text-base font-semibold ml-4">
+                <h3 className="ml-4 text-sm font-semibold text-black lg:text-base">
                   {item.name}
                 </h3>
               </div>
@@ -31,27 +31,27 @@ const Home = () => (
         </div>
       </AppSection>
       <AppSection title="Skill's & Technology">
-        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+        <div className="grid grid-cols-2 gap-3 lg:grid-cols-3 xl:grid-cols-4">
           {USER.skills.map((item) => (
             <div
               key={item.id}
-              className="flex items-center py-3 px-3 border border-light-gray rounded-lg"
+              className="flex items-center px-3 py-3 border rounded-lg border-light-gray"
             >
               <Image
                 src={item.icon}
                 alt={item.name}
-                className="w-6 md:w-8 h-6 md:h-8"
+                className="w-6 h-6 md:w-8 md:h-8"
                 width={30}
                 height={30}
               />
-              <h3 className="text-black text-xs md:text-sm font-medium ml-2  md:ml-3">
+              <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">
                 {item.name}
               </h3>
             </div>
           ))}
         </div>
       </AppSection>
-      <AppSection title="Projects's i have Complated">
+      <AppSection title="Projects's I have Completed">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-6">
           {USER.project.contents
             .filter((filteredItem) => filteredItem.top)
@@ -61,7 +61,7 @@ const Home = () => (
         </div>
         <div className="text-center py-7">
           <Link href="/projects" passHref>
-            <a className="text-xs md:text-sm text-gray transition duration-300 hover:text-orange">
+            <a className="text-xs transition duration-300 md:text-sm text-gray hover:text-orange">
               Show all my projects <i className="fas fa-chevron-right" />
             </a>
           </Link>
