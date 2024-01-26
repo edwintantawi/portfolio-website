bffcf6d98064517184cc8885551ffd43c726c202
edwintantawi <edwintantawi.dev@gmail.com>
2024-01-26 16:04:04 +0700

    remove react-reveal

--- components/atomics/AppSide.jsx
+++ components/atomics/AppSide.jsx
@@ -1,49 +1,44 @@
-import Fade from 'react-reveal/Fade';
 import USER from '../../data/user.json';
 
 const AppSide = () => (
   <div className="hidden md:flex justify-between fixed bottom-0 left-0 right-0 z-10 w-full px-3 md:px-10 2xl:px-20 max-w-[1905px] mx-auto">
-    <Fade bottom>
-      <aside className="flex flex-col items-center">
-        <ul className="flex flex-col items-center w-6 space-y-4">
-          {USER.contact.contents
-            .filter((filteredItem) => filteredItem.category === 'Social Media')
-            .map((item) => (
-              <li key={item.id} className="w-max">
-                <a
-                  href={item.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title={item.name}
-                  aria-label={item.name}
-                >
-                  <i
-                    className={`${item.icon} text-2xl transition duration-300 text-gray hover:text-primary transform hover:-translate-y-1`}
-                  />
-                </a>
-              </li>
-            ))}
-        </ul>
-        <div className="h-28 w-[2px] bg-primary mt-5" />
-      </aside>
-    </Fade>
-    <Fade bottom>
-      <aside className="flex flex-col justify-end items-center">
-        {[USER.contact.contents.find((item) => item.category === 'Email')].map(
-          (item) => (
-            <a
-              key={item.id}
-              href={`mailto:${item.username}`}
-              className="text-gray transition duration-300 hover:text-primary tracking-widest"
-              style={{ writingMode: 'tb-rl' }}
-            >
-              {item.username}
-            </a>
-          )
-        )}
-        <div className="h-28 w-[2px] bg-primary mt-5" />
-      </aside>
-    </Fade>
+    <aside className="flex flex-col items-center">
+      <ul className="flex flex-col items-center w-6 space-y-4">
+        {USER.contact.contents
+          .filter((filteredItem) => filteredItem.category === 'Social Media')
+          .map((item) => (
+            <li key={item.id} className="w-max">
+              <a
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                title={item.name}
+                aria-label={item.name}
+              >
+                <i
+                  className={`${item.icon} text-2xl transition duration-300 text-gray hover:text-primary transform hover:-translate-y-1`}
+                />
+              </a>
+            </li>
+          ))}
+      </ul>
+      <div className="h-28 w-[2px] bg-primary mt-5" />
+    </aside>
+    <aside className="flex flex-col justify-end items-center">
+      {[USER.contact.contents.find((item) => item.category === 'Email')].map(
+        (item) => (
+          <a
+            key={item.id}
+            href={`mailto:${item.username}`}
+            className="text-gray transition duration-300 hover:text-primary tracking-widest"
+            style={{ writingMode: 'tb-rl' }}
+          >
+            {item.username}
+          </a>
+        )
+      )}
+      <div className="h-28 w-[2px] bg-primary mt-5" />
+    </aside>
   </div>
 );
 
--- components/molecules/AppHero.jsx
+++ components/molecules/AppHero.jsx
@@ -1,4 +1,3 @@
-import Fade from 'react-reveal/Fade';
 import DATA from '../../data/user.json';
 import AppContainer from '../atomics/AppContainer';
 import AppButton from '../atomics/AppButton';
@@ -6,24 +5,22 @@ import AppButton from '../atomics/AppButton';
 const AppHero = () => (
   <header className="bg-hero-pattern bg-no-repeat bg-center px-3">
     <AppContainer>
-      <Fade duration={2000}>
-        <div className="h-screen flex flex-col justify-center pt-16 items-center text-center lg:items-start lg:text-left">
-          <span className="text-xs md:text-sm text-black flex items-center">
-            <span>Hi, I&apos;m</span>
-            <span className="block w-6 lg:w-11 h-1 bg-primary ml-2 rounded-lg" />
-          </span>
-          <h1 className="text-5xl lg:text-8xl font-bold text-black">
-            {DATA.name}
-          </h1>
-          <h2 className="text-xl lg:text-4xl font-bold text-primary mt-2">
-            [{DATA.status}]
-          </h2>
-          <p className="text-xs lg:text-base text-gray max-w-md tracking-widest mt-2 lg:mt-3 md:ml-1">
-            {DATA.description}
-          </p>
-          <AppButton title="GET IN TOUCH" href="/contact" />
-        </div>
-      </Fade>
+      <div className="h-screen flex flex-col justify-center pt-16 items-center text-center lg:items-start lg:text-left">
+        <span className="text-xs md:text-sm text-black flex items-center">
+          <span>Hi, I&apos;m</span>
+          <span className="block w-6 lg:w-11 h-1 bg-primary ml-2 rounded-lg" />
+        </span>
+        <h1 className="text-5xl lg:text-8xl font-bold text-black">
+          {DATA.name}
+        </h1>
+        <h2 className="text-xl lg:text-4xl font-bold text-primary mt-2">
+          [{DATA.status}]
+        </h2>
+        <p className="text-xs lg:text-base text-gray max-w-md tracking-widest mt-2 lg:mt-3 md:ml-1">
+          {DATA.description}
+        </p>
+        <AppButton title="GET IN TOUCH" href="/contact" />
+      </div>
     </AppContainer>
   </header>
 );
